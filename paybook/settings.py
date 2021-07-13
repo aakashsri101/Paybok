dc4d5caed17cde70c390c781b7bbae4dc0361110
Aakash Srivastava <aakash.sribv.101@gmail.com>
2021-07-13 20:51:52 +0530

    Update settings.py

--- paybook/settings.py
+++ paybook/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'paybook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        
- }
 }
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        
+#  }
+# }
 
 DATABASES['default']=dj_database_url.config(default='')
 db_from_env = dj_database_url.config(conn_max_age=600)
